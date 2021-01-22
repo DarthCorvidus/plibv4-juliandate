ad6d52a420c74541fad4f3978e58956df411ffbe
DarthCorvidus <github@vm01.telton.de>
2021-01-22 21:05:06 +0100

    Initial commit of JulianDate.

---
+++ src/JulianDate.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Julian Date class
+ * 
+ * JulianDate is pure date only, using julian days. JulianDate is immutable, ie.
+ * no function changes the internal state of an JulianDate instance, but returns
+ * a new instance of JulianDate instead.
+ *
+ * @author Claus-Christoph Küthe
+ * @copyright (c) 2020, Claus-Christoph Küthe
+ */
+class JulianDate {
+	const DAY = 1;
+	const WEEK = 2;
+	const MONTH = 3;
+	const YEAR = 4;
+	/** @var int Date as Julian days */
+	private $numeric;
+	/**
+	 * 
+	 * @param int $year Year
+	 * @param int $month Month
+	 * @param int $day Day
+	 */
+	function __construct(int $year=NULL, int $month=NULL, int $day=NULL) {
+		if($year==NULL)	{
+			$now = time();
+			$day = date("d", $now);
+			$month = date("m", $now);
+			$year = date("Y", $now);
+		}
+		$this->testRange($year, $month, $day);
+		$this->numeric = gregoriantojd($month, $day, $year);
+	}
+	
+	/**
+	 * Tests if months and days are valid; doesn't allow for dates like
+	 * 2020-06-31 or 2021-02-29.
+	 * @param int $year Year
+	 * @param int $month Month
+	 * @param int $day Day
+	 * @throws RangeException
+	 */
+	private function testRange(int $year, int $month, int $day) {
+		if($month<1 || $month>12) {
+			throw new RangeException("month is out of range");
+		}
+		if($day<1) {
+			throw new RangeException("day is out of range");
+		}
+		if($day<=28) {
+			return;
+		}
+		$date = new JulianDate($year, $month, 1);
+		if($date->getFormat("t")<$day) {
+			throw new RangeException("day is out of range");
+		}
+	}
+	
+	/**
+	 * Creates a date from an ISO 6801 compliant string (YYYY-MM-DD)
+	 * @param String $string Date as ISO 6801
+	 * @return JulianDate
+	 * @throws InvalidArgumentException
+	 */
+	static function fromString(String $string): JulianDate {
+		$tmp = explode("-", $string);
+		if(!preg_match("/[0-9]*-[0-9]{2}-[0-9]{2}/", $string)) {
+			throw new InvalidArgumentException("invalid isodate, must be YYYY-MM-DD");	
+		}
+		if(!preg_match("/[0-9]+-[0-9]{2}-[0-9]{2}/", $string)) {
+			throw new InvalidArgumentException("invalid isodate, must be YYYY-MM-DD");	
+		}
+	return new JulianDate($tmp[0], $tmp[1], $tmp[2]);
+	}
+	
+	/**
+	 * Constructs instance of JulianDate directly from julian days.
+	 * 
+	 * @param int $julian
+	 * @return JulianDate
+	 */
+	static function fromNumeric(int $julian): JulianDate {
+		$date = new JulianDate();
+		$date->numeric = $julian;
+	return $date;
+	}
+	
+	/**
+	 * 
+	 * @param int $unit
+	 * @throws InvalidArgumentException
+	 */
+	private function allowedUnit(int $unit) {
+		if(!in_array($unit, array(self::DAY, self::WEEK, self::MONTH, self::YEAR))) {
+			throw new InvalidArgumentException("\$unit does not contain an allowed unit");
+		}
+	}
+
+	/**
+	 * Get first day of a given unit.
+	 * @param int $unit
+	 * @return \JulianDate
+	 */
+	function getFirstOf(int $unit): JulianDate {
+		if($unit==self::DAY) {
+			return $this;
+		}
+		if($unit==self::WEEK) {
+			$days = $this->numeric-($this->getFormat("N")-1);
+		return JulianDate::fromNumeric($days);
+		}
+
+		if($unit==self::MONTH) {
+			$array = cal_from_jd($this->numeric, CAL_GREGORIAN);
+		return new JulianDate($array["year"], $array["month"], 1);
+		}
+
+		if($unit==self::YEAR) {
+			$array = cal_from_jd($this->numeric, CAL_GREGORIAN);
+		return new JulianDate($array["year"], 1, 1);
+		}
+	}
+	
+	/**
+	 * Get last day of a given unit
+	 * @param int $unit
+	 * @return \JulianDate
+	 */
+	function getLastOf(int $unit): JulianDate {
+		if($unit==self::DAY) {
+			return $this;
+		}
+		if($unit==self::WEEK) {
+			$days = $this->numeric+(7-$this->getFormat("N"));
+		return JulianDate::fromNumeric($days);
+		}
+
+		if($unit==self::MONTH) {
+			$array = cal_from_jd($this->numeric, CAL_GREGORIAN);
+			return new JulianDate($array["year"], $array["month"], (int)$this->getFormat("t"));
+		}
+
+		if($unit==self::YEAR) {
+			$array = cal_from_jd($this->numeric, CAL_GREGORIAN);
+			return new JulianDate($array["year"], 1, 1);
+		}
+	}
+	
+	/**
+	 * Get date in custom format.
+	 * @param string $format
+	 * @return string
+	 */
+	function getFormat(string $format): string {
+		$time = strtotime($this->__toString());
+	return date($format, $time);
+	}
+
+	/**
+	 * Adds a specific Unit
+	 * @param int $amount
+	 * @param int $unit
+	 * @return \JulianDate
+	 */
+	function addUnit(int $amount, int $unit): JulianDate {
+		if($amount==0) {
+			return $this;
+		}
+		$this->allowedUnit($unit);
+		if($unit==self::WEEK) {
+			return JulianDate::fromNumeric($this->numeric+($amount*7));
+		}
+		if($unit==self::MONTH) {
+			return $this->addMonths($amount);
+		}
+		if($unit==self::YEAR) {
+			return new JulianDate($this->getFormat("Y")+$amount, $this->getFormat("n"), $this->getFormat("j"));
+		}
+	return JulianDate::fromNumeric($this->numeric+$amount);
+	}
+	/**
+	 * 
+	 * @param int $amount Amount of Months to add.
+	 * @return \JulianDate
+	 */
+	private function addMonths(int $amount): JulianDate {
+		if($amount>0) {
+			$years = floor($amount/12)+$this->getFormat("Y");
+		} else {
+			$years = ceil($amount/12)+$this->getFormat("Y");
+		}
+		$months = ($amount%12)+$this->getFormat("n");
+	return new JulianDate($years, $months, $this->getFormat("j"));
+	}
+	
+	/**
+	 * Returns date as string (YYYY-MM-DD).
+	 * @return string
+	 */
+	function __toString(): string {
+		$array = cal_from_jd($this->numeric, CAL_GREGORIAN);
+	return sprintf("%d-%02d-%02d", $array["year"], $array["month"], $array["day"]);
+	}
+}
---
+++ tests/ImDateTest.php
@@ -0,0 +1,212 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of JulianDateTest
+ *
+ * @author hm
+ */
+
+final class JulianDateTest  extends TestCase {
+	public function testFromStringSane() {
+		$this->assertInstanceOf(JulianDate::class,	JulianDate::fromString("2020-06-12"));
+	}
+
+	public function testFromStringAntiquity() {
+		$this->assertInstanceOf(JulianDate::class, JulianDate::fromString("202-03-06"));
+	}
+
+	public function testFromStringDune() {
+		$this->assertInstanceOf(JulianDate::class, JulianDate::fromString("10191-03-06"));
+	}
+
+	public function testFromStringBogus() {
+		$this->expectException(InvalidArgumentException::class);
+		JulianDate::fromString("Bogus");
+	}
+
+	public function testFromStringMissingYear() {
+		$this->expectException(InvalidArgumentException::class);
+		JulianDate::fromString("-06-12");
+	}
+
+	public function testFromStringMissingMonth() {
+		$this->expectException(InvalidArgumentException::class);
+		JulianDate::fromString("2020--12");
+	}
+
+	public function testFromStringMissingDay() {
+		$this->expectException(InvalidArgumentException::class);
+		JulianDate::fromString("2020-06-");
+	}
+
+	public function testFromStringRange() {
+		$this->expectException(RangeException::class);
+		JulianDate::fromString("2020-06-31");
+	}
+
+	public function testConstruct() {
+		$this->assertInstanceOf(JulianDate::class, new JulianDate(2020, 6, 12));
+	}
+
+	public function testConstructRange() {
+		$this->expectException(RangeException::class);
+		new JulianDate(2020, 6, 31);
+	}
+
+	public function testConstructMonthZero() {
+		$this->expectException(RangeException::class);
+		new JulianDate(2020, 0, 31);
+	}
+
+	public function testConstructMonthOOR() {
+		$this->expectException(RangeException::class);
+		new JulianDate(2020, 13, 31);
+	}
+
+	public function testConstructDayZero() {
+		$this->expectException(RangeException::class);
+		new JulianDate(2020, 6, 0);
+	}
+
+	public function testConstructDayOOR() {
+		$this->expectException(RangeException::class);
+		new JulianDate(2020, 6, 31);
+	}
+
+	public function testConstructLeap() {
+		$date = JulianDate::fromString("2020-02-29");
+		$this->assertEquals("2020-02-29", $date->__toString());
+	}
+
+	public function testConstructLeapWrong() {
+		$this->expectException(RangeException::class);
+		new JulianDate(2019, 2, 29);
+	}
+	
+	public function testFormat() {
+		$date = JulianDate::fromString("2020-06-12");
+		$this->assertEquals("5", $date->getFormat("N"));
+	}
+	
+	public function testGetFirstDay() {
+		$date = JulianDate::fromString("2020-06-12");
+		$this->assertEquals("2020-06-12", $date->getFirstOf(JulianDate::DAY));
+	}
+
+	public function testGetFirstWeek() {
+		for($i=8;$i<=14;$i++) {
+			$date = new JulianDate(2020, 6, $i);
+			$this->assertEquals("2020-06-08", $date->getFirstOf(JulianDate::WEEK));
+		}
+	}
+
+	public function testGetFirstMonth() {
+		for($i=1;$i<=12;$i++) {
+			$date = new JulianDate(2020, $i, 8);
+			$this->assertEquals("2020-".str_pad((string)$i, 2, "0", STR_PAD_LEFT)."-01", $date->getFirstOf(JulianDate::MONTH));
+		}
+	}
+
+	public function testGetFirstYear() {
+		$date = JulianDate::fromString("2020-06-12");
+		$this->assertEquals("2020-01-01", $date->getFirstOf(JulianDate::YEAR));
+	}
+
+	public function testGetLastDay() {
+		$date = JulianDate::fromString("2020-06-12");
+		$this->assertEquals("2020-06-12", $date->getLastOf(JulianDate::DAY));
+	}
+
+	public function testGetLastWeek() {
+		for($i=8;$i<=14;$i++) {
+			$date = new JulianDate(2020, 6, $i);
+			$this->assertEquals("2020-06-14", $date->getLastOf(JulianDate::WEEK));
+		}
+	}
+	
+	public function testGetLastMonthStandard() {
+		$array = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+		foreach($array as $key => $value) {
+			$date = new JulianDate(2019, $key+1, 12);
+			$this->assertEquals("2019-".str_pad((string)($key+1), 2, "0", STR_PAD_LEFT)."-".$value, $date->getLastOf(JulianDate::MONTH));
+		}
+	}
+
+	public function testGetLastMonthLeap() {
+		$array = array(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+		foreach($array as $key => $value) {
+			$date = new JulianDate(2020, $key+1, 12);
+			$this->assertEquals("2020-".str_pad((string)($key+1), 2, "0", STR_PAD_LEFT)."-".$value, $date->getLastOf(JulianDate::MONTH));
+		}
+	}
+	
+	public function testAddDays() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2020-06-17", $date->addUnit(5, JulianDate::DAY));
+	}
+	
+	public function testAddWeeks() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2020-06-26", $date->addUnit(2, JulianDate::WEEK));
+	}
+
+	public function testAddMonths() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2020-09-12", $date->addUnit(3, JulianDate::MONTH));
+	}
+	
+	public function testAddManyMonths() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2023-08-12", $date->addUnit(38, JulianDate::MONTH));
+	}
+	
+	public function testAddMonthsRange() {
+		$date = new JulianDate(2020, 5, 31);
+		$this->expectException(RangeException::class);
+		$date->addUnit(1, JulianDate::MONTH);
+	}
+
+	
+	public function testAddYears() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2027-06-12", $date->addUnit(7, JulianDate::YEAR));
+	}
+	
+	public function testAddYearsRange() {
+		$date = new JulianDate(2020, 2, 29);
+		$this->expectException(RangeException::class);
+		$date->addUnit(1, JulianDate::YEAR);
+	}
+	
+	public function testSubDays() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2020-06-07", $date->addUnit(-5, JulianDate::DAY));
+	}
+	
+	public function testSubWeeks() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2020-05-29", $date->addUnit(-2, JulianDate::WEEK));
+	}
+	
+	public function testSubMonths() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2020-03-12", $date->addUnit(-3, JulianDate::MONTH));
+	}
+
+	public function testSubManyMonths() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2017-04-12", $date->addUnit(-38, JulianDate::MONTH));
+	}
+
+	public function testSubYears() {
+		$date = new JulianDate(2020, 6, 12);
+		$this->assertEquals("2017-06-12", $date->addUnit(-3, JulianDate::YEAR));
+	}
+}
